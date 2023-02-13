dab635a04713b8ec9e255a50530a3e49c2c755fd
udara <udaraprabath98@gmail.com>
2023-02-13 21:23:55 +0530

    vendor crud completed

--- src/pages/Venders.jsx
+++ src/pages/Venders.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState,useEffect } from 'react'
 import { Content, Card } from '../components/shared/Utils'
 import Navbar from '../components/shared/Navbar'
@@ -10,41 +11,169 @@ import { RiAddCircleLine } from 'react-icons/ri'
 import { ButtonNormal} from '../components/shared/Button'
 import Modal from '../components/shared/Modal'
 import TextInput from '../components/shared/TextInput'
-
-
+import Fileinput from '../components/shared/Fileinput'
+import baseUrl from '../utils/baseUrl'
 
 const Vender = () => {
-
+  const [formData, setFormData] = useState({
+    fullname: '',
+    username: '',
+    password:'',
+    confirmpassword:'',
+    profilepic:[],
+    email:'',
+    address_line1: '',
+    address_line2: '',
+    address_line3: '',
+    state: '',
+    city: '',
+    postal_code: ''
+   
+  })
   const [data, setData] =useState([])
+  const [modal, setModal] = useState(false)
+  const [isEdit,setIsEdit] = useState(false)
+  let id=null;
+  const setbodyFormData = (formData) => {
+    var bodyFormData = new FormData();
+    if(formData.password && formData.password==formData.confirmpassword)
+    {
+      bodyFormData.append('password', formData.password);
+    }
+    bodyFormData.append('fullName',formData.fullName);
+    bodyFormData.append('username', formData.username);
+    bodyFormData.append('profile_pic', formData.profilepic);
+    bodyFormData.append('mobile', formData.mobile);
+    bodyFormData.append('email', formData.email);
+    bodyFormData.append('address[address_line1]', formData.address_line1);
+    bodyFormData.append('address[address_line2]', formData.address_line2);
+    bodyFormData.append('address[address_line3]', formData.address_line3);
+    bodyFormData.append('address[state]', formData.state);
+    bodyFormData.append('address[city]', formData.city);
+    bodyFormData.append('address[postal_code]', formData.postal_code);
+    return bodyFormData
+  }  
+
+  const updateImg = (e) => {
+    console.log(e.target.files[0])
+    setFormData(prevState => {
+        return {...prevState, 'profilepic': e.target.files[0]}
+    });
+
+  }
+  const update = (name, e) => {
+    setFormData(prevState => {
+        return {...prevState, [name]: e.target.value}
+    });
+  }
+ 
     
-  useEffect( () => {
+
       async function fetchData() {
           try {
-              const res = await axios.get('http://localhost/vender.php'); 
+            const res = await axios.get(`${baseUrl}/vendor`);
+console.log(res)
               setData(res.data);
           } catch (err) {
               console.log(err);
           }
       }
-      fetchData();
+ 
+  useEffect(() => {
+    fetchData();
   }, []);
-
-  const [modal, setModal] = useState(false)
+  
+  
   
   const toggleModal = () => {
+    setFormData({})
+    setIsEdit(false)
     setModal(!modal)
   }
-
-  const onSave = () => {
-    alert('onsave clicked')
-  }
-
   const onCancel = () => {
     alert('oncancel clicked')
   }
+  const onSave = async () => {
+    const bodyFormData=  setbodyFormData(formData)
+    console.log(bodyFormData)
+   await axios({
+        method: "post",
+        url: `${baseUrl}/vendor`,
+        data: bodyFormData,
+        headers: {"Content-Type": "multipart/form-data"},
+    })
+        .then((response) => {
+            console.log(response.data)
+            fetchData()
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    toggleModal()
+//  toggleAlert('Data inserted Successfully')
 
+}
+
+const toUpdate = (data) =>{
+  setIsEdit(true)
+  setFormData({
+     'fullName':data.fullName,
+     'username':data.username,
+     'mobile':data.mobile,
+     'email':data.email,
+     'address_line1':data.address.address_line1,
+     'address_line2':data.address.address_line2,
+     'address_line3':data.address.address_line3,
+     'state':data.address.state,
+     'city':data.address.city, 
+     'postal_code':data.address.postal_code,
+    }
+    
+);
+sessionStorage.setItem('id',data._id)
+setModal(!modal)
+
+}
+
+const onUpdate = async () => {
+  const bodyFormData=  setbodyFormData(formData)
+  await axios({
+      method: "patch",
+      url: `${baseUrl}/vendor/${sessionStorage.getItem('id')}`,
+      data: bodyFormData,
+      headers: {"Content-Type": "multipart/form-data"},
+  })
+      .then((response) => {
+          console.log(response.data)
+          fetchData()
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
+  toggleModal()
+//  toggleAlert('Data inserted Successfully')
+
+}
+
+const toDelete = async (id) => {
+  await axios({
+      method: "delete",
+      url: `${baseUrl}/vendor/${id}`,
+      headers: {"Content-Type": "multipart/form-data"},
+  })
+      .then((response) => {
+          console.log(response.data)
+          fetchData()
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
+//  toggleAlert('Data inserted Successfully')
+
+}
 
   return (
+
     <div>
         <Sidebar />
         <Navbar />
@@ -54,83 +183,131 @@ const Vender = () => {
             <Modal 
             onClose={toggleModal}
             onCancel={onCancel}
-            onSave={onSave}
-            title='Create Shop'
+            onSave={isEdit ? onUpdate : onSave}
+            title='Create Vendor'
             width='w-1/2'
             > 
               <div className='flex gap-4'>
                 <div className= 'flex-1'>
                   <TextInput 
-                    label='First Name' 
+                    label='Full Name' 
                     border
                     borderColor='border-gray-600'
+                    value={formData.fullName} onChange={(e) => update("fullName", e)}
+                  />
+                 </div>
+               </div>  
+              <div className='flex gap-4'>
+                <div className= 'flex-1'>
+                  <TextInput 
+                    label='User Name' 
+                    border
+                    borderColor='border-gray-600'
+                    value={formData.username} onChange={(e) => update("username", e)}
                   />
                 </div>
                 <div className= 'flex-1'>
                   <TextInput 
-                    label='Last Name' 
+                    label='Mobile' 
                     border
                     borderColor='border-gray-600'
+                    value={formData.mobile} onChange={(e) => update("mobile", e)}
+                  />
+                </div>
+               </div> 
+               <div className='flex gap-4'>
+                <div className= 'flex-1'>
+                  <TextInput 
+                    label='Address Line 1' 
+                    border
+                    borderColor='border-gray-600'
+                    value={formData.address_line1} onChange={(e) => update("address_line1", e)}
+                  />
+                </div>
+                <div className= 'flex-1'>
+                  <TextInput 
+                    label='Address Line 2' 
+                    border
+                    borderColor='border-gray-600'
+                    value={formData.address_line2} onChange={(e) => update("address_line2", e)}
                   />
                 </div>
               </div>
               <div className='flex gap-4'>
                 <div className= 'flex-1'>
                   <TextInput 
-                    label='Shop Name' 
+                    label='Address Line 3' 
                     border
                     borderColor='border-gray-600'
+                    value={formData.address_line3} onChange={(e) => update("address_line3", e)}
                   />
+                </div>
+                <div className= 'flex-1'>
+                  <TextInput 
+                    label='City' 
+                    border
+                    borderColor='border-gray-600'
+                    value={formData.city} onChange={(e) => update("city", e)}
+                  />
+                </div>
+              </div>
+              <div className='flex gap-4'>
+                <div className= 'flex-1'>
+                  <TextInput 
+                    label='State' 
+                    border
+                    borderColor='border-gray-600'
+                    value={formData.state} onChange={(e) => update("state", e)}
+                  />
+                </div>
+                <div className= 'flex-1'>
+                  <TextInput 
+                    label='Postal code' 
+                    border
+                    borderColor='border-gray-600'
+                    value={formData.postal_code} onChange={(e) => update("postal_code", e)}
+                  />
+                </div>
+              </div>
+              <div className='flex gap-4'>
+                <div className= 'flex-1'>
+                  <TextInput 
+                    label='Password' 
+                    border
+                    borderColor='border-gray-600'
+                    type="password"
+                    value={formData.password} onChange={(e) => update("password", e)}
+                  />
+                </div>
+                <div className= 'flex-1'>
+                  <TextInput 
+                    label='Password Confirmation' 
+                    border
+                    type="password"
+                    borderColor='border-gray-600'
+                    value={formData.confirmpassword} onChange={(e) => update("confirmpassword", e)}
+                  />
+                </div>
+              </div>
+              <div className='flex gap-4'>
+                <div className= 'flex-1'>
+                  <Fileinput
+                    label={'Profile Picture'}
+                    multiple
+                    onChange={(e) => updateImg(e)}
+
+                   />
                 </div>
                 <div className= 'flex-1'>
                   <TextInput 
                     label='Email' 
                     border
                     borderColor='border-gray-600'
+                    value={formData.email} onChange={(e) => update("email", e)}
+
                   />
                 </div>
-              </div>
-              <div className='flex gap-4'>
-                <div className= 'flex-1'>
-                  <TextInput 
-                    label='Telephone 1' 
-                    border
-                    borderColor='border-gray-600'
-                  />
-                </div>
-                <div className= 'flex-1'>
-                  <TextInput 
-                    label='User Name' 
-                    border
-                    borderColor='border-gray-600'
-                  />
-                </div>
-              </div>
-              <div className='flex gap-4'>
-                <div className= 'flex-1'>
-                  <TextInput 
-                    label='Telephone 2' 
-                    border
-                    borderColor='border-gray-600'
-                  />
-                </div>
-                <div className= 'flex-1'>
-                  <TextInput 
-                    label='Password' 
-                    border
-                    borderColor='border-gray-600'
-                  />
-                </div>
-              </div>
-              <div className='flex gap-4'>
-                <div className= 'flex-1'>
-                  <TextInput 
-                    label='Personal Address' 
-                    border
-                    borderColor='border-gray-600'
-                  />
-                </div>
-              </div>             
+              </div>            
             </Modal>
           }
           <Card>
@@ -143,13 +320,14 @@ const Vender = () => {
             <Table>
 
               <THead>
-                <TH title={'Id'}/>
-                <TH title={'Vender Name'}/>
-                <TH title={'Shop Name'}/>
-                <TH title={'Addresse'}/>
+                <TH title={'Full Name'}/>
+                <TH title={'User Name'}/>
                 <TH title={'Email'}/>
-                <TH title={'Phone 1'}/>
-                <TH title={'Phone 2'}/>
+                <TH title={'Mobile'}/>
+                <TH title={'Address'}/>
+                <TH title={'State'}/>
+                <TH title={'City'}/>
+                <TH title={'Postal Code'}/>
                 <TH title={'Actions'}/>
               </THead>
 
@@ -157,32 +335,35 @@ const Vender = () => {
 
                 {data.map(d => {
                   return (
-                    <Row key={d.id}>
-                     <TD>
-                        { d.id }
-                      </TD>   
+                    <Row key={d._id}>
                       <TD>
-                        { d.vendername }
+                        { d.fullName }
                       </TD>
                       <TD>
-                        { d.shopname }
-                      </TD>
-                      <TD>
-                        { d.address }
+                        { d.username }
                       </TD>
                       <TD>
                         { d.email }
                       </TD>
                       <TD>
-                        { d.mobile1 }
+                        { d.mobile }
                       </TD>
                       <TD>
-                        { d.mobile2 }
+                        { d.address.address_line1 +','+d.address.address_line2+','+d.address.address_line3}
+                      </TD>
+                      <TD>
+                        { d.address.state }
+                      </TD>
+                      <TD>
+                        { d.address.city }
+                      </TD>
+                      <TD>
+                        { d.address.postal_code }
                       </TD>
                       <TD>
                         <div className='w-full h-full flex items-center justify-center gap-4'>
-                          <FaTrash className='w-3 h-3 fill-red-500 cursor-pointer'/>
-                          <PencilAltIcon className='w-4 h-4 fill-blue-500 cursor-pointer'/>
+                          <FaTrash onClick={() => toDelete(d._id)} className='w-3 h-3 fill-red-500 cursor-pointer'/>
+                          <PencilAltIcon onClick={() => toUpdate(d)} className='w-4 h-4 fill-blue-500 cursor-pointer'/>
                         </div>  
                       </TD>
                     </Row>
