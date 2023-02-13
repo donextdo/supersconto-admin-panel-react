bccbb882425816800eebd1c3d347a5a557dda824
lakshithadev <lxkshitha@gmail.com>
2023-02-13 10:51:10 +0530

    stock management

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -4,31 +4,75 @@ import Navbar from '../components/shared/Navbar'
 import Sidebar from '../components/shared/Sidebar'
 import { Table, THead, TBody, TH, Row, TD } from '../components/shared/Table'
 import axios from 'axios'
-import { ButtonSuccess } from '../components/shared/Button'
-import { RiAddCircleLine,} from 'react-icons/ri'
-
+import { ButtonSuccess, ButtonNormal } from '../components/shared/Button'
+import { FcClearFilters } from 'react-icons/fc'
+import baseUrl from '../utils/baseUrl'
+import Dropdown from '../components/shared/Dropdown'
 
 
 const Stocks = () => {
 
-  const [data, setData] = useState([])
+  const [stockData, setStockData] = useState([])
+  const [modal, setModal] = useState(false)
+  const [shops, setShops] = useState([])
+  const [filter, setFilter] = useState(null)
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await axios.get('http://localhost/stock.php');
-        setData(res.data);
-      } catch (err) {
-        console.log(err);
-      }
+    fetchData()
+    fetchShops()
+  }, [])
+
+  async function fetchData() {
+    try {
+      const { data } = await axios.get(`${baseUrl}/stock`)
+      setStockData(data)
     }
-    fetchData();
-  }, []);
+    catch (error) {
+      console.log(error)
+    }
+      
+  }
 
-  const [modal, setModal] = useState(false)
+  async function fetchShops() {
 
-  const toggleModal = () => {
-    setModal(!modal)
+    try {
+      const res = await axios.get(`${baseUrl}/shop`);
+      const shopData = res?.data.map(d => {
+          return {
+              value: d._id,
+              label: d.shop_name
+          }
+      })
+      setShops(shopData);
+    }
+    catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  const handleDropDownChange = async (selectedOption) => {
+    try {
+      const shop = selectedOption.value
+      setFilter(shop)
+      const { data } = await axios.post(`${baseUrl}/stock/filter-by-shop`,
+        {
+          shop
+        }
+      )
+
+      console.log(data)
+
+      setStockData(data)
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  const clearFilter = async () => {
+    await fetchData()
+    setFilter(null)
   }
 
   const onSave = () => {
@@ -51,10 +95,23 @@ const Stocks = () => {
 
           <div className='w-full py-4 flex gap-6'>
 
-            <ButtonSuccess onClick={toggleModal}>
-              <RiAddCircleLine className='w-5 h-5' />
-              <span>Add Stock</span>
-            </ButtonSuccess>
+            <div className='flex w-2/5'>
+              <div className='py-2 px-3 whitespace-nowrap bg-gray-600 text-white text-sm font-medium'>
+                Filter by shop
+              </div>
+
+              <div className='w-full'>
+                <Dropdown
+                    options={shops}
+                    onChange={handleDropDownChange}
+                />
+              </div>
+            </div>
+
+            <ButtonNormal onClick={clearFilter} disabled={!filter}>
+                <FcClearFilters className='w-5 h-5'/>
+                <span>Clear Filter</span>
+            </ButtonNormal>
 
           </div>
 
@@ -70,27 +127,27 @@ const Stocks = () => {
 
             <TBody>
 
-              {data.map(d => {
+              {stockData.map(stock => {
                 return (
-                  <Row key={d.id}>
+                  <Row key={stock._id}>
 
 
                     <TD>
-                      {d.shopName}
+                      {stock._id}
                     </TD>
 
                     <TD>
-                      {d.catalogName}
+                      {stock.product_name}
                     </TD>
 
                     <TD>
-                      {d.productName}
+                      {stock.shop_id.shop_name}
                     </TD>
                     <TD>
-                      {d.shopAddress}
+                      {stock.shop_id.address.city}
                     </TD>
                     <TD>
-                      {d.quantity}
+                      {stock.quantity}
                     </TD>
 
                   </Row>
