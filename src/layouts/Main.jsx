204a74d8c3ccb8e70296a494a0d70d32b701f969
Githendra McShane <githendramax@gmail.com>
2023-02-14 20:28:48 +0530

    login redirect

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect } from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
+import {Outlet, useLocation, useNavigate} from 'react-router-dom'
 import { Content, Card } from '../components/shared/Utils'
 import Navbar from '../components/shared/Navbar'
 import Sidebar from '../components/shared/Sidebar'
 
-const Main = () => {
+const Main = (props) => {
 
 const location = useLocation()
+const navigate = useNavigate()
 
 useEffect(() => {
     console.log(location.pathname)
+    const token = localStorage.getItem('token')
+    if (!token) {
+        navigate('/login', { replace: true })
+    }
 }, [])
 
   return (
