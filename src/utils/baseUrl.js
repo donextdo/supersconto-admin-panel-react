4f50f1b87a551b0894bf1ad0939083fad1cfe564
lakshithadev <lxkshitha@gmail.com>
2023-02-27 19:44:31 +0530

    change base url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,5 @@
- const baseUrl = 'http://localhost:3000/v1/api'
-//  const baseUrl = 'https://api.supersconto24.com/v1/api'
+//  const baseUrl = 'http://localhost:3000/v1/api'
+ const baseUrl = 'https://api.supersconto24.com/v1/api'
 export const clientAppUrl = 'https://supersconto24.com'
 // export const clientAppUrl = 'http://localhost:3001'
 
