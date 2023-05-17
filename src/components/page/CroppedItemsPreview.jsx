55b91a073402bce43d23e00036b898b32d87770b
Githendra McShane <githendramax@gmail.com>
2023-05-17 22:59:37 +0530

    ImageProcessor.jsx version 2

--- src/components/page/CroppedItemsPreview.jsx
+++ src/components/page/CroppedItemsPreview.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import Slider from "react-slick";
 
 function CroppedItemsPreview({cropImageUrl, handleRemove}) {
+    const settings = {
+        dots: true,
+        arrows: false,
+        infinite: false,
+        swipe: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    };
 
     return (
-        <div>
-            <div className="previewer">{cropImageUrl.length > 0 && cropImageUrl.map((url, index) =>
-                (
-                    <img key={`prev ${index}`} alt="Crop" style={{maxWidth: '250px', minWidth: '100px'}} src={url.croppedImageUrl} onClick={() =>handleRemove(url.index)}/>
-                )
-            )}</div>
+        <div className="previewer">
+            {cropImageUrl.length > 0 ?
+                <Slider {...settings}>
+                    {
+                        cropImageUrl.map((url, index) =>
+                            (
+                                <div key={`prev ${index}`}>
+                                    <img alt="Crop" style={{maxWidth: '250px', minWidth: '100px'}}
+                                         src={url.croppedImageUrl} onClick={() => handleRemove(url.index)}/>
+                                </div>
+                            )
+                        )}
+
+                </Slider>
+                : <p className="m-auto text-center opacity-50">No items</p>
+
+            }
+
         </div>
     );
 }
