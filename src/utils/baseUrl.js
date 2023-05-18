aef53bf33b50676b774f234d22001aa5f1604c71
kalanaw2 <manjitharock@gmail.com>
2023-05-18 11:23:30 +0530

    change branch

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,8 @@
 // const baseUrl = 'http://apidev.marriextransfer.com/v1/api'
-const baseUrl = 'http://api.supersconto24.com/v1/api'
-const clientAppUrl = 'http://supersconto24.com'
+// const baseUrl = 'http://api.supersconto24.com/v1/api'
+// const clientAppUrl = 'http://supersconto24.com'
+const baseUrl = "http://localhost:3000/v1/api";
+export const clientAppUrl = 'http://localhost:3001'
+
 
 export default baseUrl
