f38fdb5d702ddfdcb2fa3cd7c7fbce50f561fcb6
Thisara Thathsarani <thisararpg@gmail.com>
2023-06-21 23:25:28 +0530

    add main categori in to drop down

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,274 +1,296 @@
-import React, { useState,useEffect, useRef } from 'react'
-import { Content, Card } from '../components/shared/Utils'
-import Navbar from '../components/shared/Navbar'
-import Sidebar from '../components/shared/Sidebar'
-import { Table, THead, TBody, TH, Row, TD } from '../components/shared/Table'
-import { FaTrash } from 'react-icons/fa'
-import axios from 'axios'
-import { RiAddCircleLine } from 'react-icons/ri'
-import { ButtonNormal} from '../components/shared/Button'
-import Modal from '../components/shared/Modal'
-import TextInput from '../components/shared/TextInput'
-import baseUrl from '../utils/baseUrl'
-import { ToastContainer, toast } from 'react-toastify';
-import Dropdown from '../components/shared/Dropdown'
-import Confirm from '../components/shared/Confirm'
-import { PencilAltIcon } from '@heroicons/react/solid'
-
+import React, { useState, useEffect, useRef } from "react";
+import { Content, Card } from "../components/shared/Utils";
+import Navbar from "../components/shared/Navbar";
+import Sidebar from "../components/shared/Sidebar";
+import { Table, THead, TBody, TH, Row, TD } from "../components/shared/Table";
+import { FaTrash } from "react-icons/fa";
+import axios from "axios";
+import { RiAddCircleLine } from "react-icons/ri";
+import { ButtonNormal } from "../components/shared/Button";
+import Modal from "../components/shared/Modal";
+import TextInput from "../components/shared/TextInput";
+import baseUrl from "../utils/baseUrl";
+import { ToastContainer, toast } from "react-toastify";
+import Dropdown from "../components/shared/Dropdown";
+import Confirm from "../components/shared/Confirm";
+import { PencilAltIcon } from "@heroicons/react/solid";
 
 const Category = () => {
-  const [isEdit,setIsEdit] = useState(false)
-  let [options,setOptions]=useState([{value:'none',label:'none'}])
-  const [data, setData] =useState([])
-  const [modal, setModal] = useState(false)
-  const [confirm, setConfirm] = useState(false)
-  const [alertError, setAlertError] = useState(false)
-  const [formData,setFormData]=useState({name:null,mainCategoryName:null})
-  const [selected, setSelected] =useState([])
+  const [isEdit, setIsEdit] = useState(false);
+  let [options, setOptions] = useState([{ value: "none", label: "none" }]);
+  const [data, setData] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [confirm, setConfirm] = useState(false);
+  const [alertError, setAlertError] = useState(false);
+  const [categoryId, setCategoryId] = useState();
+  const [formData, setFormData] = useState({
+    name: null,
+    mainCategoryName: null,
+  });
+  const [selected, setSelected] = useState([]);
   const onDelete = (id) => {
-    sessionStorage.setItem('id',id)
-    setConfirm(true)
-  }
+    sessionStorage.setItem("id", id);
+    setConfirm(true);
+  };
 
-  useEffect( () => {
-     
-      fetchData();
-  },[]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
- const fetchData= async()=> {
+  const fetchData = async () => {
     try {
-      const categories=[]
-        const res = await axios.get(baseUrl+'/category/categories'); 
-       
-        setOptions([{value:'none',label:'none'}])
-        res.data.mainCategories.forEach(assign)
-        res.data.subCategories.forEach(assign)
-        setData(categories);
-        function assign(val){
-          categories.push(val)
+      const categories = [];
+      const res = await axios.get(baseUrl + "/category/categories");
+      console.log("category List:", res);
+      setOptions([{ value: "none", label: "none" }]);
+      res.data.mainCategories.forEach(assign);
+      res.data.subCategories.forEach(assign);
+      setData(categories);
+      function assign(val) {
+        categories.push(val);
 
-          if(!options.includes({value:val.name,label:val.name}))
-          {
-            setOptions(prevItems => [...prevItems,{value:val.name,label:val.name}]);
-          }
+        if (!options.includes({ value: val.name, label: val.name })) {
+          setOptions((prevItems) => [
+            ...prevItems,
+            { value: val.name, label: val.name },
+          ]);
         }
- 
-        console.log(options)
+      }
+
+      console.log(options);
     } catch (err) {
-        console.log(err);
+      console.log(err);
     }
-}
+  };
   const toggleModal = () => {
-    setModal(!modal)
-  }
-  const update = (e) => {
-   setFormData(prevState => {
-        return {...prevState, ['name']: e.target.value}
-    });  }
+    setModal(!modal);
+  };
+  const update = (e, categoryId) => {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        ["name"]: e.target.value,
+      };
+    });
+  };
 
-  const updateSelect=(option)=>{
-   setSelected(option)
-  }
-  const onSave = async() => {
-  if(selected.value=='none' || !selected.value)
-  {
-    await  axios({
-      method: "post",
-      url: `${baseUrl}/category/main-categories`,
-      data: formData,
-  })
-      .then((response) => {
-          setAlertError(false)
+  useEffect(() => {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        ["mainCategoryName"]: categoryId,
+      };
+    });
+  }, [selected, categoryId]);
 
-          console.log(response.data)
-          fetchData()
-          toggleModal()
-          return toast.success('Data inserted successfully')
+  const updateSelect = async (option) => {
+    setSelected(option);
+    const res = await axios.get(baseUrl + "/category/categories");
+
+    const selectedMainCategory = res.data.mainCategories.find(
+      (category) => category.name === option.value
+    );
+    if (selectedMainCategory) {
+      setCategoryId(selectedMainCategory?.name);
+    } else {
+      console.log("null");
+    }
+  };
+  // const updateSelect = (option) => {
+  //   setSelected(option);
+  //   console.log("selected main category: ", selected);
+  // };
+  const onSave = async () => {
+    console.log("while saving:", formData);
+    if (selected.value == "none" || !selected.value) {
+      await axios({
+        method: "post",
+        url: `${baseUrl}/category/main-categories`,
+        data: formData,
       })
-      .catch(function (error) {
+        .then((response) => {
+          setAlertError(false);
+
+          console.log("main: ", response.data);
+          fetchData();
+          toggleModal();
+          return toast.success("Data inserted successfully");
+        })
+        .catch(function (error) {
           console.log(error);
-          toggleModal()
-          return toast.error(error.message)
-      });
-  }
-    else{
-      await  axios({
+          toggleModal();
+          return toast.error(error.message);
+        });
+    } else {
+      await axios({
         method: "post",
         url: `${baseUrl}/category/sub-categories`,
         data: formData,
-    })
+      })
         .then((response) => {
-            setAlertError(false)
-            toggleModal()
+          setAlertError(false);
+          toggleModal();
 
-            console.log(response.data)
-            fetchData()
-            return toast.success('Data inserted successfully')
+          console.log("sub: ", response.data);
+          fetchData();
+          return toast.success("Data inserted successfully");
         })
         .catch(function (error) {
-            console.log(error);
-            toggleModal()
+          console.log(error);
+          toggleModal();
 
-            return toast.error(error.message)
-  
+          return toast.error(error.message);
         });
     }
-  }
-  const toUpdate = (data) =>{
-    setIsEdit(true)
-  sessionStorage.setItem('id',data._id)
-  setModal(!modal)
-  setFormData(prevState => {
-    return {...prevState, ['name']: data.name}
-});
-  }
+  };
+  const toUpdate = (data) => {
+    setIsEdit(true);
+    sessionStorage.setItem("id", data._id);
+    setModal(!modal);
+    setFormData((prevState) => {
+      return { ...prevState, ["name"]: data.name };
+    });
+  };
   const onCancel = () => {
-    setModal(!modal)
-  }
+    setModal(!modal);
+  };
 
   const toDelete = async (id) => {
     await axios({
-        method: "delete",
-        url: `${baseUrl}/category/categories/${sessionStorage.getItem('id')}`,
-        headers: {"Content-Type": "multipart/form-data"},
+      method: "delete",
+      url: `${baseUrl}/category/categories/${sessionStorage.getItem("id")}`,
+      headers: { "Content-Type": "multipart/form-data" },
     })
-        .then((response) => {
-            console.log(response.data)
-            setAlertError(false)
-            fetchData()
-            return toast.success('Data Deleted Successfully')
-  
-        })
-        .catch(function (error) {
-            console.log(error);
-            setAlertError(true)
-            console.log(error);
-            return toast.error(error.message)
-        });
-        setConfirm(false)
-  
-  //  toggleAlert('Data inserted Successfully')
-  
-  }
+      .then((response) => {
+        console.log(response.data);
+        setAlertError(false);
+        fetchData();
+        return toast.success("Data Deleted Successfully");
+      })
+      .catch(function (error) {
+        console.log(error);
+        setAlertError(true);
+        console.log(error);
+        return toast.error(error.message);
+      });
+    setConfirm(false);
+
+    //  toggleAlert('Data inserted Successfully')
+  };
   const onUpdate = async () => {
     await axios({
-        method: "put",
-        url: `${baseUrl}/category/categories/${sessionStorage.getItem('id')}`,
-        data: formData,
+      method: "put",
+      url: `${baseUrl}/category/categories/${sessionStorage.getItem("id")}`,
+      data: formData,
     })
-        .then((response) => {
-            console.log(response.data)
-            setAlertError(false)
-            fetchData()
-            return  toast.success('Data Updated Successfully')
-  
-        })
-        .catch(function (error) {
-            console.log(error);
-            setAlertError(true)
-            console.log(error);
-            return toast.error('Something went Wrong')
-        });
-    toggleModal()
-  //  toggleAlert('Data inserted Successfully')
-  setIsEdit(false)
-  setFormData({})
-
-    }
-    const nameRef=useRef()
+      .then((response) => {
+        console.log(response.data);
+        setAlertError(false);
+        fetchData();
+        return toast.success("Data Updated Successfully");
+      })
+      .catch(function (error) {
+        console.log(error);
+        setAlertError(true);
+        console.log(error);
+        return toast.error("Something went Wrong");
+      });
+    toggleModal();
+    //  toggleAlert('Data inserted Successfully')
+    setIsEdit(false);
+    setFormData({});
+  };
+  const nameRef = useRef();
   return (
     <div>
-        {/* <Sidebar />
+      {/* <Sidebar />
         <Navbar />
 
         <Content> */}
-        <>
-        <ToastContainer/>
-        {confirm &&
-        <Confirm
+      <>
+        <ToastContainer />
+        {confirm && (
+          <Confirm
             onSave={toDelete}
-            onCancel={()=>setConfirm(false)}
-            onClose={()=>setConfirm(false)}
-
-        >
-        </Confirm>}
-        {modal && 
-            <Modal 
+            onCancel={() => setConfirm(false)}
+            onClose={() => setConfirm(false)}
+          ></Confirm>
+        )}
+        {modal && (
+          <Modal
             onClose={toggleModal}
             onCancel={onCancel}
             onSave={isEdit ? onUpdate : onSave}
-            title='Categories'
-            width='w-1/2'
-            height='h-2/3'
-            > 
-              <div className='flex gap-4'>
-                <div className= 'flex-1'>
-                  <Dropdown 
-                    label='Main Category' 
-                    border
-                    value={selected?selected:options[0]}
-                    borderColor='border-gray-600'
-                    options={options}
-                    onChange={updateSelect}
-                  />
-                </div>
+            title="Categories"
+            width="w-1/2"
+            height="h-2/3"
+          >
+            <div className="flex gap-4">
+              <div className="flex-1">
+                <Dropdown
+                  label="Main Category"
+                  border
+                  value={selected ? selected : options[0]}
+                  borderColor="border-gray-600"
+                  options={options}
+                  onChange={updateSelect}
+                />
               </div>
-              <div className='flex gap-4'>
-                <div className= 'flex-1'>
-                  <TextInput 
-                    label='Sub Category/Category Name' 
-                    border
-                    borderColor='border-gray-600'
-                    value={formData.name} onChange={(e) => update(e)}
-                  />
-                </div>
-              </div>
-            </Modal>
-          }
-          <Card>
-            <div className='w-full py-4 flex gap-6'>
-                <ButtonNormal onClick={toggleModal}>
-                  <RiAddCircleLine className='w-5 h-5'/>
-                  <span>Add</span>
-                </ButtonNormal>
             </div>
-            <Table>
+            <div className="flex gap-4">
+              <div className="flex-1">
+                <TextInput
+                  label="Sub Category/Category Name"
+                  border
+                  borderColor="border-gray-600"
+                  value={formData.name}
+                  onChange={(e) => update(e)}
+                />
+              </div>
+            </div>
+          </Modal>
+        )}
+        <Card>
+          <div className="w-full py-4 flex gap-6">
+            <ButtonNormal onClick={toggleModal}>
+              <RiAddCircleLine className="w-5 h-5" />
+              <span>Add</span>
+            </ButtonNormal>
+          </div>
+          <Table>
+            <THead>
+              <TH title={"Name"} />
 
-              <THead>
-                <TH title={'Name'}/>
-               
-                <TH width={'w-[20px]'} title={'Actions'}/>
-              </THead>
+              <TH width={"w-[20px]"} title={"Actions"} />
+            </THead>
 
-              <TBody>
-
-                {data.map(d => {
-                  return (
-                    <Row key={d._id}>
-                      <TD>
-                        { d.name }
-                      </TD>
-                      <TD>
-                        <div className='w-full h-full flex items-center justify-center gap-4'>
-                          <FaTrash onClick={() => onDelete(d._id)} className='w-3 h-3 fill-red-500 cursor-pointer'/>
-                          <PencilAltIcon onClick={() => toUpdate(d)} className='w-4 h-4 fill-blue-500 cursor-pointer'/>
-                        </div>  
-                      </TD>
-                    </Row>
-                  )
-                })
-
-                }
-
-              </TBody>
-
-            </Table>
-
-          </Card>
-        </>
-        {/* </Content> */}
+            <TBody>
+              {data.map((d) => {
+                return (
+                  <Row key={d._id}>
+                    <TD>{d.name}</TD>
+                    <TD>
+                      <div className="w-full h-full flex items-center justify-center gap-4">
+                        <FaTrash
+                          onClick={() => onDelete(d._id)}
+                          className="w-3 h-3 fill-red-500 cursor-pointer"
+                        />
+                        <PencilAltIcon
+                          onClick={() => toUpdate(d)}
+                          className="w-4 h-4 fill-blue-500 cursor-pointer"
+                        />
+                      </div>
+                    </TD>
+                  </Row>
+                );
+              })}
+            </TBody>
+          </Table>
+        </Card>
+      </>
+      {/* </Content> */}
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- src/pages/PageItems.jsx
+++ src/pages/PageItems.jsx
@@ -1,176 +1,191 @@
-import React, {useEffect, useState} from 'react'
-import {useLocation, useNavigate} from 'react-router-dom';
-import {Link} from 'react-router-dom';
-import Navbar from '../components/shared/Navbar';
-import Sidebar from '../components/shared/Sidebar';
-import {Content, Card} from '../components/shared/Utils';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
+import Sidebar from "../components/shared/Sidebar";
+import { Content, Card } from "../components/shared/Utils";
 import axios from "axios";
 import ImageProcessor from "../components/page/ImageProcessor.jsx";
-import Modal from '../components/shared/Modal'
-import AddPageItems from '../components/page_items/AddPageItems'
-import baseUrl from '../utils/baseUrl'
-import {FaAngleLeft} from 'react-icons/fa'
+import Modal from "../components/shared/Modal";
+import AddPageItems from "../components/page_items/AddPageItems";
+import baseUrl from "../utils/baseUrl";
+import { FaAngleLeft } from "react-icons/fa";
 
-import image from '../assets/flyer_1.jpg'
-
+import image from "../assets/flyer_1.jpg";
 
 const PageItems = (props) => {
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  const shop = query.get("shop");
+  const catelog = query.get("catelog");
+  const page = query.get("page");
+  const [pageData, setPageData] = useState(null);
+  const [exCoordinates, setExCoordinates] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [pageItem, setPageItem] = useState({
+    shop_id: shop,
+    catelog_book_id: catelog,
+    catelog_page_id: page,
+    product_name: "",
+    product_category: "",
+    product_sub_category: "",
+    product_description: "",
+    item_index: 0,
+    crop_id: 0,
+    quantity: 0,
+    unit_price: 0,
+    online_sell: false,
+    product_image: image, //SET THIS IN IMAGE PROCESSOR
+    coordinates: null,
+  });
+  const navigate = useNavigate();
+  useEffect(() => {
+    console.log("api-call");
+    fetchData(page)
+      .then((res) => {
+        setPageData(res?.data);
+        setExCoordinates(
+          res?.data?.items
+            .map((it) => ({ ...it.coordinates, id: it._id }))
+            .flatMap((a) => a) ?? []
+        );
+      })
+      .catch((error) => {
+        // TODO
+        console.log(error);
+      });
+  }, []);
 
-    const location = useLocation();
-    const query = new URLSearchParams(location.search);
-    const shop = query.get('shop');
-    const catelog = query.get('catelog');
-    const page = query.get('page')
-    const [pageData, setPageData] = useState(null)
-    const [exCoordinates, setExCoordinates] = useState([])
-    const [modal, setModal] = useState(false)
-    const [pageItem, setPageItem] = useState({
-        shop_id: shop,
-        catelog_book_id: catelog,
-        catelog_page_id: page,
-        product_name: '',
-        product_category: '',
-        product_description: '',
-        item_index: 0,
-        crop_id: 0,
-        quantity: 0,
-        unit_price: 0,
-        online_sell: false,
-        product_image:  image, //SET THIS IN IMAGE PROCESSOR
-        coordinates: null
-    })
-    const navigate = useNavigate();
-    useEffect(() => {
-        console.log('api-call')
-        fetchData(page).then(res => {
-            setPageData(res?.data)
-            setExCoordinates(res?.data?.items.map(it => ({...it.coordinates, id: it._id})).flatMap(a => a) ?? [])
-        }).catch(error => {
-            // TODO
-            console.log(error)
+  async function fetchData(pageId) {
+    return axios.get(`${baseUrl}/catelog/page/find/${pageId}`);
+  }
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const handleSave = async (payload) => {
+    const { crop } = payload;
+    let extra;
+    console.log({ payload });
+    if (crop.id) {
+      console.log("crop has id", pageData.items);
+      extra = { ...crop, ...pageData?.items.find((it) => it._id === crop.id) };
+    }
+    console.log({ crop });
+    setPageItem((prevState) => ({
+      ...prevState,
+      product_image: crop.croppedImageUrl,
+      coordinates: crop,
+      ...extra,
+    }));
+    toggleModal();
+  };
+
+  const handleSaveProduct = async (rest) => {
+    try {
+      const res = await axios.post(`${baseUrl}/product/insert`, rest);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onSave = async () => {
+    try {
+      const { product_image, ...rest } = pageItem;
+      const formData = new FormData();
+      formData.append(
+        "product_image",
+        await blobToFile(product_image, `${pageItem.product_name}.jpeg`)
+      );
+      formData.append("data", JSON.stringify(rest));
+      console.log({ formData, product_image, rest });
+
+      handleSaveProduct(rest);
+
+      axios
+        .post(`${baseUrl}/catelog/item`, formData)
+        .then(() => {
+          fetchData(page)
+            .then((res) => {
+              setPageData(res?.data);
+              setExCoordinates(
+                res?.data?.items
+                  .map((it) => ({
+                    ...it.coordinates,
+                    id: it._id,
+                  }))
+                  .flatMap((a) => a) ?? []
+              );
+            })
+            .catch((error) => {
+              // TODO
+              console.log(error);
+            });
         })
-    }, [])
 
-    async function fetchData(pageId) {
-        return axios.get(`${baseUrl}/catelog/page/find/${pageId}`);
+        .catch((error) => {
+          // TODO
+          console.log(error);
+        });
+
+      toggleModal();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const toggleModal = () => {
-        setModal(!modal)
-    }
+  async function blobToFile(theBlob, fileName) {
+    const response = await fetch(theBlob);
+    const blob = await response.blob();
+    return new File([blob], fileName);
+    // return new File([theBlob], fileName, {lastModified: new Date().getTime(), type: theBlob.type})
+  }
 
-    const handleSave = async (payload) => {
-        const {crop} = payload
-        let extra
-        console.log({payload})
-        if (crop.id) {
-            console.log("crop has id", pageData.items)
-            extra = {...crop, ...pageData?.items.find(it => it._id === crop.id)}
-        }
-        console.log({crop})
-        setPageItem(prevState => ({...prevState, product_image: crop.croppedImageUrl, coordinates: crop, ...extra}))
-        toggleModal()
-    }
+  const onCancel = () => {
+    toggleModal();
+  };
 
-    const handleSaveProduct = async (rest) => {
-        try {
-
-            const res = await axios.post(`${baseUrl}/product/insert`,rest)
-            console.log(res.data)
-
-      
-          } catch (error) {
-            console.log(error)
-          }
-
-    }
-
-    const onSave = async () => {
-        try {
-            const {product_image, ...rest} = pageItem
-            const formData = new FormData();
-            formData.append('product_image', await blobToFile(product_image, `${pageItem.product_name}.jpeg`))
-            formData.append('data', JSON.stringify(rest))
-            console.log({formData, product_image, rest})
-
-            handleSaveProduct(rest)
-
-            axios.post(`${baseUrl}/catelog/item`, formData).then(() => {
-                fetchData(page).then(res => {
-                    setPageData(res?.data)
-                    setExCoordinates(res?.data?.items.map(it => ({
-                        ...it.coordinates,
-                        id: it._id
-                    })).flatMap(a => a) ?? [])
-                }).catch(error => {
-                    // TODO
-                    console.log(error)
-                })
-            })
-
-            .catch(error => {
-                // TODO
-                console.log(error)
-            })
-
-            toggleModal()
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    async function blobToFile(theBlob, fileName) {
-
-        const response = await fetch(theBlob);
-        const blob = await response.blob();
-        return new File([blob], fileName);
-        // return new File([theBlob], fileName, {lastModified: new Date().getTime(), type: theBlob.type})
-    }
-
-    const onCancel = () => {
-        toggleModal()
-    }
-
-
-    console.log('page-items-rendered', {pageData, pageItem})
-    return (
-        <div>
-            {/* <Navbar screen/>
+  console.log("page-items-rendered", { pageData, pageItem });
+  return (
+    <div>
+      {/* <Navbar screen/>
             <Sidebar minimize/> */}
 
-            <button className="text-4xl pl-20 fixed z-50 left-6 top-4" onClick={() =>  navigate(-1)}><FaAngleLeft /></button>
+      <button
+        className="text-4xl pl-20 fixed z-50 left-6 top-4"
+        onClick={() => navigate(-1)}
+      >
+        <FaAngleLeft />
+      </button>
 
+      {pageData && (
+        <div>
+          {modal && (
+            <Modal
+              width="w-[60vw]"
+              height="h-[40vw]"
+              onClose={toggleModal}
+              onCancel={onCancel}
+              onSave={onSave}
+              title="Add page product"
+            >
+              <AddPageItems pageItem={pageItem} setPageItem={setPageItem} />
+            </Modal>
+          )}
 
-            {pageData && <div>
-
-                {modal &&
-                    <Modal
-                        width='w-[60vw]'
-                        height='h-[40vw]'
-                        onClose={toggleModal}
-                        onCancel={onCancel}
-                        onSave={onSave}
-                        title='Add page product'>
-
-                        <AddPageItems pageItem={pageItem} setPageItem={setPageItem}/>
-
-                    </Modal>
-                }
-
-                <Card>
-
-                    <ImageProcessor
-                        imgSrc={pageData.page_image}
-                        exCoordinates={exCoordinates}
-                        onClick={handleSave}
-                    />
-
-                </Card>
-
-            </div>}
+          <Card>
+            <ImageProcessor
+              imgSrc={pageData.page_image}
+              exCoordinates={exCoordinates}
+              onClick={handleSave}
+            />
+          </Card>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default PageItems
+export default PageItems;
